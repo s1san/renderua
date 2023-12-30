cbd35156bb62493dbe5884e2d504547178bfff92
s1san <s1sanCN@163.com>
2023-12-30 20:23:26 +0800

    finish tinyrenderer lesson 2

--- main.cpp
+++ main.cpp
@@ -2,37 +2,35 @@
 #include <cmath>
 
 #include "tgaimage.h"
-#include "model.h"
 #include "geometry.h"
+#include "model.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
+const TGAColor green = TGAColor(0, 255, 0, 255);
 const int width = 800;
 const int height = 800;
 
 Model* model = NULL;
 
-void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
+void line(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color)
 {
 	bool steep = false;
-	if (std::abs(x0 - x1) < std::abs(y0 - y1))
+	if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y))
 	{
-		std::swap(x0, y0);
-		std::swap(x1, y1);
+		std::swap(p0.x, p0.y);
+		std::swap(p1.x, p1.y);
 		steep = true;
 	}
-	if (x0 > x1)
-	{
-		std::swap(x0, x1);
-		std::swap(y0, y1);
-	}
+	if (p0.x > p1.x)
+		std::swap(p0, p1);
 
-	int dx = x1 - x0;
-	int dy = y1 - y0;
+	int dx = p1.x - p0.x;
+	int dy = p1.y - p0.y;
 	int derror = std::abs(dy) * 2;
 	float error = 0;
-	int y = y0;
-	for (float x = x0; x <= x1; x++)
+	int y = p0.y;
+	for (float x = p0.x; x <= p1.x; x++)
 	{
 		if (steep)
 			image.set(y, x, color);
@@ -42,9 +40,65 @@ void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color)
 		error += derror;
 		if (error > dx)
 		{
-			y += (y1 > y0 ? 1 : -1);
+			y += (p1.y > p0.y ? 1 : -1);
 			error -= dx * 2;
 		}
+	}
+}
+
+Vec3f barycentric(Vec2i* pts, Vec2i P)
+{
+	Vec3f u = Vec3f(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x) ^ Vec3f(pts[2].y - pts[0].y, pts[1].y - pts[0].y, pts[0].y - P.y);
+	if (std::abs(u.z) < 1)	return Vec3f(-1, 1, 1);
+	return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+}
+
+void triangle(Vec2i* pts, TGAImage& image, TGAColor color)
+{
+	Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
+	Vec2i bboxmax(0, 0);
+	Vec2i clamp(image.get_width() - 1, image.get_height() - 1);
+	for (int i = 0; i < 3; i++)
+	{
+		bboxmin.x = std::max(0, std::min(bboxmin.x, pts[i].x));
+		bboxmin.y = std::max(0, std::min(bboxmin.y, pts[i].y));
+
+		bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+		bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+	}
+
+	Vec2i P;
+	for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
+		for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
+		{
+			Vec3f bc = barycentric(pts, P);
+			if (bc.x < 0 || bc.y < 0 || bc.z < 0) continue;
+			image.set(P.x, P.y, color);
+		}
+}
+
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color) {
+	if (t0.y == t1.y && t0.y == t2.y) return;
+
+	if (t0.y > t1.y) std::swap(t0, t1);
+	if (t0.y > t2.y) std::swap(t0, t2);
+	if (t1.y > t2.y) std::swap(t1, t2);
+
+	int total_height = t2.y - t0.y;
+	for (int i = 0; i < total_height; i++)
+	{
+		bool second_half = i > t1.y - t0.y || t1.y == t0.y;
+		int segment_height = second_half ? t2.y - t1.y : t1.y - t0.y;
+
+		float alpha = (float)i / total_height;
+		float beta = (float)(i - (second_half ? t1.y - t0.y : 0)) / segment_height;
+
+		Vec2i A = t0 + (t2 - t0) * alpha;
+		Vec2i B = second_half ? t1 + (t2 - t1) * beta : t0 + (t1 - t0) * beta;
+
+		if (A.x > B.x) std::swap(A, B);
+		for (int j = A.x; j <= B.x; j++)
+			image.set(j, t0.y + i, color);
 	}
 }
 
@@ -56,26 +110,29 @@ int main(int argc, char** argv)
 		model = new Model("african_head.obj");
 
 	TGAImage image(width, height, TGAImage::RGB);
+	Vec3f light_dir(0, 0, -1);
 
 	for (int i = 0; i < model->nfaces(); i++)
 	{
 		std::vector<int> face = model->face(i);
+		Vec2i screen_coords[3];
+		Vec3f world_coords[3];
 		for (int j = 0; j < 3; j++)
 		{
-			Vec3f v0 = model->vert(face[j]);
-			Vec3f v1 = model->vert(face[(j+1)%3]);
-			int x0 = (v0.x + 1.) * width / 2.;
-			int y0 = (v0.y + 1.) * height / 2.;
-			int x1 = (v1.x + 1.) * width / 2.;
-			int y1 = (v1.y + 1.) * height / 2.;
-			line(x0, y0, x1, y1, image, white);
+			Vec3f v = model->vert(face[j]);
+			screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+			world_coords[j] = v;
 		}
+		Vec3f n = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+		n.normalize();
+		float intensity = n * light_dir;
+		if (intensity > 0)
+			triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
 	}
+
 
 	image.flip_vertically();
 	image.write_tga_file("output.tga");
-
-	delete model;
 
 	return 0;
 }
